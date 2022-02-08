7416cc65dddb6d57329b4256206d66678373c450
Charet <2779678916@qq.com>
2022-02-09 04:37:40 +0800

    修改细节,增加websocket处理

--- Server/controller/controller.go
+++ Server/controller/controller.go
@@ -6,10 +6,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 	"net/http"
 )
 
-func LoginHandle(c *gin.Context) {
+func LoginHandler(c *gin.Context) {
 	userInfo := models.User{}
 	resp, err := c.GetRawData()
 	//创建用户对象、接收服务端json
@@ -24,7 +25,7 @@ func LoginHandle(c *gin.Context) {
 	c.JSON(http.StatusOK, logic.Login(&userInfo))
 }
 
-func RegisterHandle(c *gin.Context) {
+func RegisterHandler(c *gin.Context) {
 	userInfo := models.User{}
 	resp, err := c.GetRawData()
 	if err != nil {
@@ -38,6 +39,22 @@ func RegisterHandle(c *gin.Context) {
 	c.JSON(http.StatusOK, logic.Register(&userInfo))
 }
 
-func ChatHandle(c *gin.Context) {
-
+// WsHandler TestHandler socket 连接 中间件 作用:升级协议,用户验证,自定义信息等
+func WsHandler(c *gin.Context) {
+	username := c.Query("username")
+	friendName := c.Query("friend_id")
+	conn, err := (&websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}).Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		http.NotFound(c.Writer, c.Request)
+		return
+	}
+	//可以添加用户信息验证
+	client := &models.Client{
+		ID:     logic.CreatId(username, friendName),
+		Socket: conn,
+		Send:   make(chan []byte),
+	}
+	models.Manager.Register <- client
+	go logic.Read(client)
+	go logic.Write(client)
 }
