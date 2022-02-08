44789d1149e050e8032f5d8a5ab37210419a8de9
Charet <2779678916@qq.com>
2022-02-09 04:29:00 +0800

    WebSocket

---
+++ Server/logic/chat.go
@@ -0,0 +1,85 @@
+package logic
+
+import (
+	"NeChat/models"
+	"encoding/json"
+	"github.com/gorilla/websocket"
+	"log"
+)
+
+// WsStart is  项目运行前, 协程开启start -> go Manager.WsStart()
+func WsStart(manager *models.ClientManager) {
+	for {
+		log.Println("<---管道通信--->")
+		select {
+		case conn := <-models.Manager.Register:
+			log.Printf("新用户加入:%v", conn.ID)
+			models.Manager.Clients[conn.ID] = conn
+			jsonMessage, _ := json.Marshal(&models.Message{Content: "Successful connection to socket service"})
+			conn.Send <- jsonMessage
+		case conn := <-models.Manager.Unregister:
+			log.Printf("用户离开:%v", conn.ID)
+			if _, ok := models.Manager.Clients[conn.ID]; ok {
+				jsonMessage, _ := json.Marshal(&models.Message{Content: "A socket has disconnected"})
+				conn.Send <- jsonMessage
+				close(conn.Send)
+				delete(models.Manager.Clients, conn.ID)
+			}
+		case message := <-models.Manager.Broadcast:
+			MessageStruct := models.Message{}
+			json.Unmarshal(message, &MessageStruct)
+			models.AddMessage(&MessageStruct)
+			for id, conn := range models.Manager.Clients {
+				if id != CreatId(MessageStruct.Recipient, MessageStruct.Sender) {
+					continue
+				}
+				select {
+				case conn.Send <- message:
+				default:
+					close(conn.Send)
+					delete(models.Manager.Clients, conn.ID)
+				}
+			}
+		}
+	}
+}
+func CreatId(uid, fid string) string {
+	return uid + "_" + fid
+}
+func Read(c *models.Client) {
+	defer func() {
+		models.Manager.Unregister <- c
+		c.Socket.Close()
+	}()
+
+	for {
+		c.Socket.PongHandler()
+		_, message, err := c.Socket.ReadMessage()
+		if err != nil {
+			models.Manager.Unregister <- c
+			c.Socket.Close()
+			break
+		}
+		log.Printf("读取到客户端的信息:%s", string(message))
+		models.Manager.Broadcast <- message
+	}
+}
+
+func Write(c *models.Client) {
+	defer func() {
+		c.Socket.Close()
+	}()
+
+	for {
+		select {
+		case message, ok := <-c.Send:
+			if !ok {
+				c.Socket.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+			log.Printf("发送到到客户端的信息:%s", string(message))
+
+			c.Socket.WriteMessage(websocket.TextMessage, message)
+		}
+	}
+}
---
+++ Server/models/chat.go
@@ -0,0 +1,47 @@
+package models
+
+import (
+	"NeChat/dao"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"time"
+)
+
+// ClientManager is a websocket manager
+type ClientManager struct {
+	Clients    map[string]*Client
+	Broadcast  chan []byte
+	Register   chan *Client
+	Unregister chan *Client
+}
+
+// Client is a websocket client
+type Client struct {
+	ID     string
+	Socket *websocket.Conn
+	Send   chan []byte
+}
+
+// Message is return msg
+type Message struct {
+	Sender    string `json:"sender,omitempty"`
+	Recipient string `json:"recipient,omitempty"`
+	Content   string `json:"content,omitempty"`
+	Date      time.Time
+}
+
+// Manager define a ws server manager
+var Manager = ClientManager{
+	Broadcast:  make(chan []byte),
+	Register:   make(chan *Client),
+	Unregister: make(chan *Client),
+	Clients:    make(map[string]*Client),
+}
+
+func AddMessage(message *Message) {
+	sqlStr := "INSERT INTO user_infos (sender, recipient, content, date) VALUES (?, ?, ?, ?)"
+	_, err := dao.DB.Exec(sqlStr, message.Sender, message.Recipient, message.Content, message.Date)
+	if err != nil {
+		fmt.Println("[ERROR]Inset data(message) failed,", err)
+	}
+}
